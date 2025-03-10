e0ab027a737c2d3ebe09a0da4ccddc136a26084d
pratikmaheta <pratikmaheta989@gmail.com>
2025-03-10 14:33:45 +0530

    Add files via upload

---
+++ module-2-advanced-javascript/15-if.js
@@ -0,0 +1,14 @@
+// if: if is a conditional statements
+// if is executed when condition is true
+let x=20;
+let y=10;
+// sytax
+// if(condition)
+// {
+//     statements;
+// }
+
+if(x>y)
+{
+    console.log('x is greter than y');
+}
---
+++ module-2-advanced-javascript/19-if-elseif.js
@@ -0,0 +1,30 @@
+
+ /* if else if: if is executed when condition is true elseif is checked multiple true condition elsif is false else is executed
+  let x = 10;
+  let y = 15;
+  syntax;
+  if (condition) {
+      statements;
+    }
+   elseif(condition) {
+    statements;
+  }
+  else 
+  {
+  statements;
+  }
+
+  */
+  let x = 30;
+  let y = 30;
+  if (x > y)
+  {
+    console.log("x is greater than y");
+  }
+  else if(b>a)
+  {
+    console.log("y is greater than x");
+  }
+ else {
+    console.log("x and y both are equal numbers");
+  }
